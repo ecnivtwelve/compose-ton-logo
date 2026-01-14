795f4b1eb6d39b3ff4cc3f996cab708f5f262890
Vince Linise <contact@vincelinise.com>
2026-01-14 12:01:28 +0100

    fix: Fixes

--- src/renderer/src/components/LogoGallery.jsx
+++ src/renderer/src/components/LogoGallery.jsx
@@ -15,6 +15,7 @@ const LogoItem = React.memo(function LogoItem({ logo }) {
         overflow: 'hidden',
         position: 'relative',
         scale: 1.6,
+        filter: 'drop-shadow(0px 0px 4px rgba(0, 0, 0, 0.5))',
         // Performance hints
         willChange: 'transform',
         contain: 'layout style paint'
